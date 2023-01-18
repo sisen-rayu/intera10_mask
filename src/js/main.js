18067aab3a09f82b1544571e25b8596d711a9199
chihiro.tanigawa <chihiro.tanigawa@accenture.com>
2023-01-19 02:41:52 +0900

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,10 @@
 import '../scss/style.scss'
 
 // import test from './modules/test';
+
+const $img = document.querySelector('img');
+
+window.addEventListener('scroll', () => {
+  const current = window.scrollY;
+  $img.style.clipPath = `circle(${current + 50}px at center)`;
+});
